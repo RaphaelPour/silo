e2d059915d23ae2b48840327471b231c63f5e6db
Raphael Pour <info@raphaelpour.de>
2024-02-01 22:52:07 +0100

    testing: add struct test

--- direct_test.go
+++ direct_test.go
@@ -21,3 +21,24 @@ func TestCRUD(t *testing.T) {
 	_, err = store.Get("a")
 	require.ErrorIs(t, ErrKeyNotExist, err)
 }
+
+type TestStruct struct {
+	number int
+	text   string
+	list   []int
+}
+
+func TestDirectStruct(t *testing.T) {
+	input := TestStruct{
+		1337, "I am here", []int{3, 2, 1},
+	}
+
+	store := NewDirect()
+	require.NoError(t, store.Set("key", input))
+
+	rawValue, err := store.Get("key")
+	require.NoError(t, err)
+	actual, ok := rawValue.(TestStruct)
+	require.True(t, ok)
+	require.Equal(t, input, actual)
+}
