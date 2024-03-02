8af42d85bd07e2c02a50a002cfd86edf7bbdde9b
Raphael Pour <info@raphaelpour.de>
2024-03-02 14:18:18 +0100

    cache: add kv layer with access to format layer

---
+++ cache.go
@@ -0,0 +1,34 @@
+package silo
+
+type Cache struct {
+	parent FormatLayer
+	m      map[string]any
+}
+
+func NewCache(parent FormatLayer) KeyValueLayer {
+	return Cache{
+		parent: parent,
+	}
+}
+
+func (c Cache) Get(key string) (any, error) {
+	var err error
+	if c.m == nil {
+		c.m, err = c.parent.Read()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return c.m[key], nil
+}
+
+func (c Cache) Set(key string, value any) error {
+	c.m[key] = value
+	return c.parent.Write(c.m)
+}
+
+func (c Cache) Delete(key string) error {
+	delete(c.m, key)
+	return c.parent.Write(c.m)
+}
